147eeee300b34a4139bbd3b1e9110ee930053c8a
aliianza12 <juanv9664@gmail.com>
2020-05-06 15:47:59 -0500

    initial commit

---
+++ app/src/main/java/com/example/ormroom/CreateContact.java
@@ -0,0 +1,35 @@
+package com.example.ormroom;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class CreateContact extends AppCompatActivity {
+    @BindView(R.id.firstNameEditText)
+    EditText firstNameEditText;
+    @BindView(R.id.lastNameEditText)
+    EditText lastNameEditText;
+    @BindView(R.id.phoneNumberEditText)
+    EditText phoneNumberEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_contact);
+        ButterKnife.bind(this);//inyeccion de dependencia gracias al BindView
+    }
+
+    @OnClick(R.id.saveButton)
+    public void onViewClicked() {
+
+        setResult(RESULT_OK);
+        finish();
+
+    }
+}
+
+
